9794d6b0b35a307e242cff0183aca6c51f2eb3f3
Divyanshu Dobhal <dobhaldivyanshu5@gmail.com>
2026-05-29 00:44:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 
 
-
 from flask import Flask, render_template, request, jsonify, redirect, url_for, flash, send_file
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
